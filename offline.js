2d6277a1d638ef1583a0e234cce622e1f99f401f
zhangyudaliwan <162434379+zhangyudaliwan@users.noreply.github.com>
2025-04-27 13:42:39 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,15 @@
+﻿{
+	"version": 1745731978,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/xueshan.png",
+		"images/diban.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground3.png",
+		"images/tiledbackground4.png",
+		"images/tiledbackground5.png"
+	]
+}
